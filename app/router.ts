8630a7e7315d7dd21e433e8deff9a122cb0d92f5
唐道海 <tangdaohai@outlook.com>
2020-01-30 00:40:22 +0800

    feat(app/router.ts): 设置 /api/v1 路由前缀

--- app/router.ts
+++ app/router.ts
@@ -2,6 +2,8 @@ import { Application } from 'egg'
 
 export default (app: Application) => {
   const { controller, router } = app
+  // 设置前缀
+  const v1 = router.prefix('/api/v1')
 
-  router.post('/user/login', controller.user.signIn)
+  v1.post('/user/login', controller.user.signIn)
 }
