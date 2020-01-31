a16625c1011acc48da8d3f5e351eafc7c9861f2e
唐道海 <tangdaohai@outlook.com>
2020-01-31 19:05:24 +0800

    feat(model/user): mongoose schema

--- app/model/user.ts
+++ app/model/user.ts
@@ -3,9 +3,15 @@ export default app => {
   const Schema = mongoose.Schema
 
   const UserSchema = new Schema({
-    id: { type: String },
-    username: { type: String },
-    password: { type: String }
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: String,
+    identity: String,
+    sex: Number,
+    avatar: String,
+    area: String,
+    createTime: { type: Date, required: true },
+    updateTime: { type: Date, required: true }
   })
 
   return mongoose.model('user', UserSchema)
