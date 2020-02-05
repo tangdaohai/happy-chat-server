771f580fbc974f6537d5e694cf71ba266f5ed2ac
唐道海 <tangdaohai@outlook.com>
2020-02-05 23:51:36 +0800

    refactor(middleware/jwt): 增加注释, 修改 status => 401

--- app/middleware/jwt.ts
+++ app/middleware/jwt.ts
@@ -1,24 +1,32 @@
 import { Context, EggAppConfig } from 'egg'
 import { verify } from 'jsonwebtoken'
-
+/**
+ * Json Web Token
+ */
 export default (options: EggAppConfig['jwt']): any => {
   return async function (ctx: Context, next) {
+    // 判断白名单，如果 url 匹配 则忽略 token 验证
     if (options.ignore.includes(ctx.url)) {
       await next()
       return
     }
+    // 不存在 token
     if (!ctx.headers.token) {
       ctx.helper.result(false, '请登录获取授权')
-      ctx.status = 403
+      ctx.status = 401
       return
     }
     try {
+      // decode token
       const decoded: any = verify(ctx.headers.token, options.secretKey)
+      // 将解密后的 uid 挂载到 ctx 上面
       ctx._uid = decoded._uid
       await next()
     } catch (e) {
-      ctx.logger.error(e)
-      ctx.status = 403
+      // 验证失败
+      ctx.logger.error(`${e.name}: ${e.message} - ${e.expiredAt}`)
+      ctx.helper.result(false, '无效的 token')
+      ctx.status = 401
     }
   }
 }
