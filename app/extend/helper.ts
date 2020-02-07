8456a542c5297444dff6e5a412fa8dba45e84b61
唐道海 <tangdaohai@outlook.com>
2020-02-07 18:47:31 +0800

    feat(extend/helper): 增加 success & error 的 body 返回格式

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,9 +1,25 @@
 import { IHelper } from 'egg'
 export default {
+  /**
+   * 设置返回的 ctx.body
+   */
   result (this: IHelper, success: boolean, content?: any) {
     this.ctx.body = {
       success,
       content
     }
+  },
+
+  /**
+   * 设置返回 成功 的 ctx.body
+   */
+  success (this: IHelper, content?: any) {
+    this.result(true, content)
+  },
+  /**
+   * 设置返回 失败 的 ctx.body
+   */
+  error (this: IHelper, content?: any) {
+    this.result(false, content)
   }
 }
