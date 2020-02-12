dc0717e206698076d2cb07ca4a95110dd6e2a90c
唐道海 <tangdaohai@outlook.com>
2020-02-12 21:00:16 +0800

    feat(service/friend): save add request

--- app/service/Friend.ts
+++ app/service/Friend.ts
@@ -29,4 +29,39 @@ export default class FriendService extends Service {
     // @TODO result 排序等相关逻辑
     return result
   }
+
+  /**
+   * 储存添加好友的请求信息
+   *
+   * @param {string} uid 发起者 id
+   * @param {string} friendId 被添加的 id
+   * @param {string} source 来源（群、搜索id 号）
+   * @param {string} reqMsg 请求添加的消息
+   * @return {object} 添加记录
+   * @memberof FriendService
+   */
+  public async saveAddRequest (uid: string, friendId: string, source: string, reqMsg: string) {
+    const model = this.app.model
+    // 查询当前 uid 对应的 name
+    const User = await model.User.findById(uid)
+    // 如果 name 不存在 使用 uid 代替
+    const name = User?.name || uid
+    // 请求时间
+    const now = new Date()
+    // 整理请求记录 并返回调用者
+    const hsitory = {
+      uid,
+      friendId,
+      friendName: name,
+      source,
+      msg: [{
+        from: '',
+        msg: reqMsg
+      }],
+      requestTime: now
+    }
+    // 储存
+    await model.AddFriendHistory.create(hsitory)
+    return hsitory
+  }
 }
